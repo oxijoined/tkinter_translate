b0b040358a26af31ab646a3995e2f3323ec534f2
okcu <dev@okcuu.ru>
2023-06-27 17:35:06 +0300

    db

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from googletrans import Translator
+import sqlite3
 
 translator = Translator()
 
@@ -34,8 +35,52 @@ languages = {
     "Болгарский": "bg",
     "Словацкий": "sk",
     "Словенский": "sl",
-    "Латинский": "la"
+    "Латинский": "la",
 }
+
+
+class Database:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+        self.cursor = None
+
+    def connect(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+
+    def disconnect(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
+
+    def create_table(self):
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS translations (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                source_language TEXT,
+                target_language TEXT,
+                source_text TEXT,
+                translated_text TEXT
+            )
+            """
+        )
+        self.conn.commit()
+
+    def insert_translation(
+        self, source_language, target_language, source_text, translated_text
+    ):
+        self.cursor.execute(
+            """
+            INSERT INTO translations (source_language, target_language, source_text, translated_text)
+            VALUES (?, ?, ?, ?)
+            """,
+            (source_language, target_language, source_text, translated_text),
+        )
+        self.conn.commit()
+
 
 def translate():
     selected_language = comboTwo.get()
@@ -44,6 +89,11 @@ def translate():
         translation = translator.translate(text, dest=languages[selected_language])
         output_text.delete("1.0", END)
         output_text.insert("1.0", translation.text)
+        # Вставляем перевод в базу данных
+        db.insert_translation(
+            comboOne.get(), comboTwo.get(), text.strip(), translation.text.strip()
+        )
+
 
 root = Tk()
 root.geometry("500x350")
@@ -77,4 +127,15 @@ translate_button.pack()
 output_text = Text(root, width=35, height=5, font="Arial 12 bold")
 output_text.pack(pady=20)
 
+# Создаем объект базы данных
+db = Database("translations.db")
+# Подключаемся к базе данных
+db.connect()
+# Создаем таблицу, если она не существует
+db.create_table()
+
+
 root.mainloop()
+
+# Отключаемся от базы данных после закрытия окна
+db.disconnect()
